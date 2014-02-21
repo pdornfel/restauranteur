f09c4280240bd4d345ca4a39f79561ecce203955
Paul Dornfeld <pdornfel@gmail.com>
2014-02-21 15:59:22 -0500

    added angular javascript. removed turbolinks. setup application.html file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Restauranteur::Application.routes.draw do
+
+  get "static_pages/index"
+
   resources :restaurants
+
+  root'static_pages#index'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
